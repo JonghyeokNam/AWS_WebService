0a450dc6861d03b848fa1a460d55837e71e86971
JonghyeokNam <njh9750@naver.com>
2025-01-28 16:55:26 +0900

    first commit

---
+++ freelec-springboot-websevice/src/main/java/com/jojoldu/book/freelec_springboot_websevice/FreelecSpringbootWebseviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelec_springboot_websevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootWebseviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootWebseviceApplication.class, args);
+	}
+
+}
---
+++ freelec-springboot-websevice/src/test/java/com/jojoldu/book/freelec_springboot_websevice/FreelecSpringbootWebseviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelec_springboot_websevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebseviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
